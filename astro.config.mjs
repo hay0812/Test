7268d9d18c0e90f7b02587f365f6dacae6561777
x9mdq7dj4v-lgtm <x9mdq7dj4v@privaterelay.appleid.com>
2026-06-17 23:55:01 +0900

    少し修正。canonicalの最適化とastro.config.mjsの修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
 adapter: cloudflare({
-    
+    output: 'static',
+    site: "https://backup-status.heyete.net",
     platformProxy: {
       enabled: true,
     },
